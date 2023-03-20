a7bb975294ad26d9e0cc5661820b6f85d0f32cb6
ahmeteminkara <emin9457@gmail.com>
2023-03-20 23:40:37 +0300

    [feature/room] init room

---
+++ app/src/main/java/com/aek/artbook/repository/room/BaseDao.kt
@@ -0,0 +1,26 @@
+package com.aek.artbook.repository.room
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Update
+
+@Dao
+interface BaseDao<T> {
+
+    @Insert
+    suspend fun insert(item: List<T>)
+
+    @Insert
+    suspend fun insert(item: T)
+
+    @Delete
+    suspend fun delete(item: T)
+
+    @Update
+    suspend fun update(item: T)
+
+    suspend fun getAll(): List<T>
+
+    suspend fun getWithId(id: Int): T
+}
